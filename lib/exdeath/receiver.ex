f64d343c9fb362935aef87aef7d081d6a729b296
kobatako <ju.koba.itit.shar@gmail.com>
2019-03-07 22:33:06 +0900

    listen socket without receiver

--- lib/exdeath/receiver.ex
+++ lib/exdeath/receiver.ex
@@ -1,30 +1,18 @@
 defmodule Exdeath.Receiver do
   use GenServer
 
-  def start_link() do
+  def start_link(listen) do
     IO.inspect "start Exdeath Receiver"
-    ip = {0, 0, 0, 0}
-    port = 4040
-    GenServer.start_link(__MODULE__, [{ip, port}], name: __MODULE__)
+    GenServer.start_link(__MODULE__, [listen], name: __MODULE__)
   end
 
-  def init([{ip, port}]) do
-    with {:ok, listen_socket} <- :gen_tcp.listen(port, [:binary, packet: :raw, active: true, ip: ip])
-    do
-      :gen_server.cast(self(), :accept)
-      {:ok, %{ip: ip, port: port, listen: listen_socket, front: nil, back: nil}}
-    else
-      {:error, :eaddrinuse} ->
-        {:stop, {:shutdown, :eaddrinuse}}
-      {:error, reason} ->
-        {:stop, {:shutdown, reason}}
-      error ->
-        {:stop, {:shutdown, error}}
-    end
+  def init([listen]) do
+    :gen_server.cast(self(), :accept)
+    {:ok, %{listen: listen, front: nil, back: nil}}
   end
 
   def handle_cast(:accept, %{listen: listen}=state) do
-    with {:ok, socket} = :gen_tcp.accept(listen)
+    with {:ok, socket} <- :gen_tcp.accept(listen)
     do
       {:noreply, %{state| front: socket}}
     else
@@ -37,7 +25,7 @@ defmodule Exdeath.Receiver do
     end
   end
 
-  def handle_info({:tcp, socket, packet}, %{front: socket}=state) do
+  def handle_info({:tcp, socket, packet}, %{front: socket, back: nil}=state) do
     IO.inspect "receive front socket "
     IO.inspect socket
     packets = String.split(packet, "\r\n")
@@ -45,6 +33,15 @@ defmodule Exdeath.Receiver do
     {:ok, con} = :gen_tcp.connect({192, 168, 33, 61}, 8010, [:binary, packet: 0])
     :gen_tcp.send(con, packet)
     {:noreply, %{state| back: con}}
+  end
+
+  def handle_info({:tcp, socket, packet}, %{front: socket, back: back}=state) do
+    IO.inspect "receive front socket "
+    IO.inspect socket
+    packets = String.split(packet, "\r\n")
+    IO.inspect packets
+    :gen_tcp.send(back, packet)
+    {:noreply, state}
   end
 
   def handle_info({:tcp, socket, packet}, %{front: front, back: socket}=state) do
